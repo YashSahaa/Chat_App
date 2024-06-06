d1b7f50c51618fa90292ac1c68ab3cb815fdbe8c
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-06-06 12:13:47 +0530

    server completed

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const sockerIoServer = socketIo.Server;
 
 
 const app = express(); // we were invoking express and here app is an express server
+app.use(express.static('frontend')) //this piece of code help us to send static files
+
 const httpVersionOfApp = http.createServer(app); // it takes server and it gives http version of server
 //socketio works with http version that's why we need to create http version of our app
 
+const io = new sockerIoServer(httpVersionOfApp); //io is just a instance of socket server 
+//on is like an addEventListener means they are not similar but anology is same
+io.on('connection',(socket)=>{ //connection is pre defined message
+    socket.on('sending message',(data)=>{
+        io.emit('io spreading message',data); //emit is actually performing an event , this is similar to click
+    }); // here sending message is a custom event
+});
 
 
 httpVersionOfApp.listen(3000); //it will give port to server
